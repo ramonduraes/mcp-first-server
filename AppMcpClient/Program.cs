f9fcab6c2a72c741d64c83d8b812490c9239907f
Ramon Durães <ramonduraes@outlook.com>
2025-06-06 18:58:56 -0300

    Adding Companion Projects: AppMcpClient / AppMcpClientStdio

---
+++ AppMcpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using ModelContextProtocol.Client;
+
+Console.WriteLine("Initializing MCP Client...");
+
+var transportOptions = new SseClientTransport(new SseClientTransportOptions
+{
+    
+    Name = "MCP Client",
+    Endpoint = new Uri("http://localhost:5000/mcp")
+});
+
+// Create the MCP client with the specified transport
+var mcpClient = await McpClientFactory.CreateAsync(transportOptions);
+// Retrieve the list of available tools
+var availableTools = await mcpClient.ListToolsAsync();
+
+
+// Display available tools
+Console.WriteLine("Available Tools:");
+foreach (var tool in availableTools)
+{
+    Console.WriteLine($"Name: {tool.Name}");
+    Console.WriteLine($"Description: {tool.Description}");
+    Console.WriteLine($"Schema: {tool.JsonSchema}");
+    Console.WriteLine(new string('-', 30));  
+}
+
+// Find the "say" tool
+var sayTool = availableTools
+    .FirstOrDefault(t => string.Equals(t.Name, "say", StringComparison.OrdinalIgnoreCase)) ??
+     throw new InvalidOperationException("The 'say' tool is not available.");
+
+// Prepare parameters for the "say" tool
+var parameters = new Dictionary<string, object?> { { "name", "ramon" } };
+
+// Call the "say" tool with the parameters
+var response = await mcpClient.CallToolAsync(sayTool.Name, parameters);
+
+// Process the response
+var result = response.Content?.FirstOrDefault()?.Text ?? "No return";
+Console.WriteLine($"Tool response: {result}");
+
+
+
+
+
---
+++ AppMcpClientStdio/Program.cs
@@ -0,0 +1,31 @@
+﻿using ModelContextProtocol.Client;
+
+Console.WriteLine("Initializing MCP Stdio Client...");
+
+var transportOptions = new StdioClientTransport(new StdioClientTransportOptions
+{
+    Name = "MCP Stdio Client",
+    Command = "dotnet",
+    Arguments = new[] { "run", "--project", @"..\AppMcpServer" }
+});
+
+// Create the MCP client with the specified transport
+var mcpClient = await McpClientFactory.CreateAsync(transportOptions);
+// Retrieve the list of available tools
+var availableTools = await mcpClient.ListToolsAsync();
+
+
+// Display available tools
+Console.WriteLine("Available Tools:");
+foreach (var tool in availableTools)
+{
+    Console.WriteLine($"Name: {tool.Name}");
+    Console.WriteLine($"Description: {tool.Description}");
+    Console.WriteLine($"Schema: {tool.JsonSchema}");
+    Console.WriteLine(new string('-', 30));  
+}
+
+
+
+
+
